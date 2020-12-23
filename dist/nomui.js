9c518547b29548eb44352dda76703d12711bb0e5
jerry <jerryzhouxx@gmail.com>
2020-12-23 14:24:22 +0800

    Textarea 组件 dom 事件响应函数用箭头函数。

--- dist/nomui.js
+++ dist/nomui.js
@@ -5128,19 +5128,19 @@
       _config() {
           this.setProps({
               attrs: {
-                  'oninput'() {
+                  'oninput': () => {
                       if (!this.capsLock) {
                           this.multilineTextbox._onValueChange();
                       }
                   },
-                  'oncompositionstart'() {
+                  'oncompositionstart': () => {
                       this.capsLock = true;
                   },
-                  'oncompositionend'() {
+                  'oncompositionend': () => {
                       this.capsLock = false;
                       this.element.trigger('input');
                   },
-                  'onblur'() {
+                  'onblur': () => {
                       this.multilineTextbox.trigger("blur");
                   }
               }
