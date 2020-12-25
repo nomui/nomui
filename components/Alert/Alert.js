765c9de5031c2636c03b7da3e14d9e8c330b14b8
xiexingen <1002275364@qq.com>
2020-12-25 15:00:30 +0800

    去掉;

--- components/Alert/Alert.js
+++ components/Alert/Alert.js
@@ -3,31 +3,31 @@ import Modal, { ModalDialog } from '../Modal/index'
 import AlertContent from './AlertContent'
 
 class Alert extends Modal {
-    constructor(props, ...mixins) {
-        const defaults = {
-            type: 'default',
-            icon: null,
-            title: null,
-            description: null
-        }
-
-        super(Component.extendProps(defaults, props), ...mixins)
+  constructor(props, ...mixins) {
+    const defaults = {
+      type: 'default',
+      icon: null,
+      title: null,
+      description: null,
     }
 
-    _config() {
-        this.setProps({
-            children: {
-                component: ModalDialog,
-                children: {
-                    component: AlertContent,
-                    type: this.props.type,
-                    icon: this.props.icon,
-                    title: this.props.title,
-                    description: this.props.description
-                }
-            }
-        })
-    }
+    super(Component.extendProps(defaults, props), ...mixins)
+  }
+
+  _config() {
+    this.setProps({
+      children: {
+        component: ModalDialog,
+        children: {
+          component: AlertContent,
+          type: this.props.type,
+          icon: this.props.icon,
+          title: this.props.title,
+          description: this.props.description,
+        },
+      },
+    })
+  }
 }
 
 Component.register(Alert)
--- components/Alert/AlertContent.js
+++ components/Alert/AlertContent.js
@@ -2,39 +2,36 @@ import Component from '../Component/index'
 import { isString } from '../util/index'
 
 class AlertContent extends Component {
-    constructor(props, ...mixins) {
-        const defaults = {
-            title: null,
-            description: null,
-            icon: null,
-            type: null
-        }
-
-        super(Component.extendProps(defaults, props), ...mixins)
+  constructor(props, ...mixins) {
+    const defaults = {
+      title: null,
+      description: null,
+      icon: null,
+      type: null,
     }
 
-    _config() {
-        var title = this.props.title;
-        if (isString(title)) {
-            title = {
-                children: this.props.title
-            }
-        };
+    super(Component.extendProps(defaults, props), ...mixins)
+  }
 
-        var description = this.props.description;
-        if (isString(description)) {
-            description = {
-                children: this.props.description
-            }
-        };
-
-        this.setProps({
-            children: [
-                title,
-                description
-            ]
-        })
+  _config() {
+    let { title } = this.props
+    if (isString(title)) {
+      title = {
+        children: this.props.title,
+      }
     }
+
+    let { description } = this.props
+    if (isString(description)) {
+      description = {
+        children: this.props.description,
+      }
+    }
+
+    this.setProps({
+      children: [title, description],
+    })
+  }
 }
 
 Component.register(AlertContent)
