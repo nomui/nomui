a43f0b0e46025f3b342ad87d97605574cc7d5c1c
fatalisa <dan.su@ctsmed.com>
2020-12-29 11:36:59 +0800

    zc

---
+++ components/Badge/Badge.js
@@ -0,0 +1,83 @@
+import Component from '../Component/index'
+
+class Badge extends Component {
+  constructor(props, ...mixins) {
+    const defaults = {
+      tag: 'span',
+      type: 'square',
+      text: null,
+      icon: null,
+      number: null,
+    }
+
+    super(Component.extendProps(defaults, props), ...mixins)
+  }
+
+  _config() {
+    this._propStyleClasses = ['size']
+    const { icon, text, type, number } = this.props
+
+    if (icon) {
+      this.setProps({
+        classes: {
+          'p-with-icon': true,
+        },
+      })
+    }
+
+    if (type === 'round') {
+      this.setProps({
+        classes: {
+          'u-shape-round': true,
+        },
+      })
+    } else if (type === 'dot') {
+      if (number > 0) {
+        this.setProps({
+          classes: {
+            'p-with-number': true,
+          },
+        })
+      }
+    }
+
+    this.setProps({
+      children: [
+        Component.normalizeIconProps(icon),
+        text && { tag: 'span', children: text },
+        number && { tag: 'span', children: number },
+      ],
+    })
+  }
+
+  _disable() {
+    this.element.setAttribute('disabled', 'disabled')
+  }
+}
+
+Component.mixin({
+  _config: function () {
+    if (this.props.badge) {
+      this.setProps({
+        classes: {
+          's-with-badge': true,
+        },
+      })
+    }
+  },
+  _render: function () {
+    if (this.props.badge) {
+      const badgeProps = {
+        type: 'dot',
+      }
+      badgeProps.number = this.props.badge.number ? this.props.badge.number : null
+      badgeProps.styles = this.props.badge.styles ? this.props.badge.styles : { color: 'danger' }
+      this.props.badge = badgeProps
+      this.badge = new Badge(Component.extendProps({ reference: this }, this.props.badge))
+    }
+  },
+})
+
+Component.register(Badge)
+
+export default Badge
---
+++ components/Badge/demos/basic.js
@@ -0,0 +1,55 @@
+define([], function () {
+  return {
+    title: '基础用法',
+    file: 'basic',
+    demo: function () {
+      return {
+        children: [
+          {
+            component: 'Badge',
+            text: '默认',
+          },
+          {
+            component: 'Badge',
+            text: '带图标',
+            icon: 'plus',
+            styles: {
+              color: 'primary',
+            },
+          },
+          {
+            component: 'Badge',
+            text: '带数字',
+            number: '5',
+          },
+          {
+            component: 'Badge',
+            text: '圆形',
+            type: 'round',
+          },
+          {
+            component: 'Button',
+            text: '按钮带圆点',
+            badge: true,
+          },
+          {
+            component: 'Button',
+            text: '按钮带圆点数字',
+            badge: {
+              number: 115,
+            },
+          },
+          {
+            component: 'Button',
+            text: '自定义角标颜色',
+            badge: {
+              styles: {
+                color: 'success',
+              },
+            },
+          },
+        ],
+      }
+    },
+  }
+})
