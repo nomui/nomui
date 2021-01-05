d165c149525ec7fbedfacc8fe29c49b94c7156e9
xiexingen <1002275364@qq.com>
2021-01-05 16:19:08 +0800

    完善AvatarGroup以及api md

--- docs/components/demo.js
+++ docs/components/demo.js
@@ -1,19 +1,17 @@
 define(['/docs/DemoPanel.js'], function (demoPanel) {
+  return {
+    component: demoPanel,
+    autoRender: false,
+    _create: function () {
+      const { type, cat, demo } = this.$route.query
+      let url = `/components/${type}/demos/${demo}.js`
+      if (cat) {
+        url = `/components/${type}/demos/${cat}/${demo}.js`
+      }
 
-    return {
-        component: demoPanel,
-        autoRender: false,
-        _create: function () {
-
-            let { type, cat, demo } = this.$route.query
-            let url = `/components/${type}/demos/${demo}.js`
-            if (cat) {
-                url = `/components/${type}/demos/${cat}/${demo}.js`
-            }
-
-            require([url], (props) => {
-                this.update(props);
-            })
-        },
-    }
+      require([url], (props) => {
+        this.update(props)
+      })
+    },
+  }
 })
