1b29a927bf9f4e07bf262c007b894ac533de218b
jerry <galford666@163.com>
2021-01-27 22:34:09 +0800

    调整字段及字段的样式

--- components/Group/demos/basic.js
+++ components/Group/demos/basic.js
@@ -4,30 +4,43 @@ define([], function () {
         file: 'basic',
         demo: function () {
             return {
-                component: 'Group',
-                fields: [
+                component: 'Rows',
+                items: [
                     {
-                        component: 'Textbox', name: 'name', label: '姓名',
+                        component: 'Form',
+                        fields: [
+                            {
+                                component: 'Textbox', name: 'name', label: '姓名',
+                            },
+                        ]
                     },
                     {
-                        component: 'Numberbox', name: 'age', label: '年龄',
-                    },
-                    {
-                        component: 'RadioList', name: 'gender', label: '性别',
-                        options: [
-                            { text: '男', value: 0 },
-                            { text: '女', value: 1 },
+                        component: 'Group',
+                        fields: [
+                            {
+                                component: 'Textbox', name: 'name', label: '姓名',
+                            },
+                            {
+                                component: 'Numberbox', name: 'age', label: '年龄',
+                            },
+                            {
+                                component: 'RadioList', name: 'gender', label: '性别',
+                                options: [
+                                    { text: '男', value: 0 },
+                                    { text: '女', value: 1 },
+                                ],
+                            },
+                            {
+                                component: 'CheckboxList', name: 'hobbies', label: '爱好',
+                                options: [
+                                    { text: '唱歌', value: 1 },
+                                    { text: '跳舞', value: 2 },
+                                    { text: '旅游', value: 3 },
+                                ],
+                            },
                         ],
-                    },
-                    {
-                        component: 'CheckboxList', name: 'hobbies', label: '爱好',
-                        options: [
-                            { text: '唱歌', value: 1 },
-                            { text: '跳舞', value: 2 },
-                            { text: '旅游', value: 3 },
-                        ],
-                    },
-                ],
+                    }
+                ]
             }
         },
     }
