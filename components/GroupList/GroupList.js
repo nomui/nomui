b2134d607be94c0f1a21477c496ae916d3323bde
jerry <galford666@163.com>
2021-02-27 21:57:01 +0800

    GroupList 新增 addDefaultValue 属性用来配置添加时的默认值

--- components/GroupList/GroupList.js
+++ components/GroupList/GroupList.js
@@ -1,83 +1,91 @@
 import Component from '../Component/index'
 import Group from '../Group/Group'
+import { isFunction } from '../util/index'
 
 class GroupList extends Group {
-    constructor(props, ...mixins) {
-        const defaults = {
-            fieldDefaults: { component: Group }
-        }
-
-        super(Component.extendProps(defaults, props), ...mixins)
+  constructor(props, ...mixins) {
+    const defaults = {
+      fieldDefaults: { component: Group },
     }
 
-    _config() {
-        const that = this
-        const { groupDefaults, value } = this.props
-        const extGroupDefaults = Component.extendProps(groupDefaults, {
-            _config: function () {
-                const group = this
-                this.setProps({
-                    action: [{
-                        component: 'Button',
-                        text: '移除',
-                        onClick: () => {
-                            group.remove()
-                            that._onValueChange()
-                        }
-                    }]
-                })
-            }
-        })
+    super(Component.extendProps(defaults, props), ...mixins)
+  }
 
-        const groups = []
-        if (Array.isArray(value)) {
-            value.forEach(function (item) {
-                groups.push(Component.extendProps(extGroupDefaults, { value: item }))
-            })
-        }
-
+  _config() {
+    const that = this
+    const { groupDefaults, value, addDefaultValue } = this.props
+    const extGroupDefaults = Component.extendProps(groupDefaults, {
+      _config: function () {
+        const group = this
         this.setProps({
-            fields: groups,
-            fieldDefaults: extGroupDefaults,
-            controlAction: [{
-                component: 'Button',
-                type: 'dashed',
-                text: '添加',
-                span: 12,
-                block: true,
-                onClick: () => {
-                    that.appendField(extGroupDefaults)
-                    that._onValueChange()
-                }
-            }],
+          action: [
+            {
+              component: 'Button',
+              text: '移除',
+              onClick: () => {
+                group.remove()
+                that._onValueChange()
+              },
+            },
+          ],
         })
+      },
+    })
 
-        super._config()
+    const groups = []
+    if (Array.isArray(value)) {
+      value.forEach(function (item) {
+        groups.push(Component.extendProps(extGroupDefaults, { value: item }))
+      })
     }
 
-    getValue() {
-        const value = []
-        for (let i = 0; i < this.fields.length; i++) {
-            const field = this.fields[i]
-            if (field.getValue) {
-                const fieldValue = field.getValue()
-                value.push(fieldValue)
-            }
+    this.setProps({
+      fields: groups,
+      fieldDefaults: extGroupDefaults,
+      controlAction: [
+        {
+          component: 'Button',
+          type: 'dashed',
+          text: '添加',
+          span: 12,
+          block: true,
+          onClick: () => {
+            extGroupDefaults.value = isFunction(addDefaultValue)
+              ? addDefaultValue.call(this)
+              : addDefaultValue
+            that.appendField(extGroupDefaults)
+            that._onValueChange()
+          },
+        },
+      ],
+    })
+
+    super._config()
+  }
+
+  getValue() {
+    const value = []
+    for (let i = 0; i < this.fields.length; i++) {
+      const field = this.fields[i]
+      if (field.getValue) {
+        const fieldValue = field.getValue()
+        value.push(fieldValue)
+      }
+    }
+
+    return value
+  }
+
+  setValue(value) {
+    if (Array.isArray(value)) {
+      for (let i = 0; i < this.fields.length; i++) {
+        const field = this.fields[i]
+        if (field.setValue) {
+          field.setValue(value[i])
         }
-
-        return value
+      }
     }
-
-    setValue(value) {
-        if (Array.isArray(value)) {
-            for (let i = 0; i < this.fields.length; i++) {
-                const field = this.fields[i]
-                if (field.setValue) {
-                    field.setValue(value[i])
-                }
-            }
-        }
-    }
+  }
 }
 
 Component.register(GroupList)
