63073c02c67b2fca5319b2091222bbf6cfc55351
Jerry <jerry.zhou@wetrial.com>
2021-03-11 17:29:00 +0800

    重写 Confirm 组件 onOk 回调逻辑

--- components/Confirm/Confirm.js
+++ components/Confirm/Confirm.js
@@ -3,26 +3,32 @@ import Modal from '../Modal/index'
 import ConfirmContent from './ConfirmContent'
 
 class Confirm extends Modal {
-    constructor(props, ...mixins) {
-        const defaults = {
-            icon: 'question-circle',
-            title: null,
-            description: null,
-            action: null,
+  constructor(props, ...mixins) {
+    const defaults = {
+      icon: 'question-circle',
+      title: null,
+      description: null,
+      action: null,
+    }
+
+    super(Component.extendProps(defaults, props), ...mixins)
+  }
+
+  _config() {
+    const { onOk } = this.props
+    this.setProps({
+      content: {
+        component: ConfirmContent,
+      },
+      onOk: (e) => {
+        if (onOk(e) !== false) {
+          e.sender.close()
         }
+      },
+    })
 
-        super(Component.extendProps(defaults, props), ...mixins)
-    }
-
-    _config() {
-        this.setProps({
-            content: {
-                component: ConfirmContent,
-            }
-        })
-
-        super._config()
-    }
+    super._config()
+  }
 }
 
 Component.register(Confirm)
