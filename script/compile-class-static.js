6c54cf8eebae909227c98304d6638b1a0dfe9895
xiexingen <1002275364@qq.com>
2021-04-08 09:02:28 +0800

    通过babel编译静态属性

--- script/compile-class-static.js
+++ script/compile-class-static.js
@@ -1,22 +1,15 @@
-const { Parser } = require('acorn')
-const staticClassFeatures = require('acorn-static-class-features')
-
-const myParse = Parser.extend(staticClassFeatures)
-
-// const testCode = myParse.parse('class X { static x = 0 }')
-// console.log(testCode)
+const babel = require('@babel/core')
 
 function compileClassStaticProps() {
   return {
     name: 'compile-class-static-props', // this name will show up in warnings and errors
     renderChunk(code) {
-      // return myParse.parse(code, {
-      //   plugins: {
-      //     jsx: false, // true to enable JSX plugin
-      //     staticClassPropertyInitializer: true, // true to enable staticClassPropertyInitializer plugin
-      //   },
-      // })
-      return myParse.parse(code)
+      return babel.transformSync(code, {
+        minified: false,
+        comments: true,
+        compact: true,
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      })
     },
   }
 }
