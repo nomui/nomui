50e576c899ffdf207f989d8d76fe849d139b2031
luoquan <quan.luo@wetrial.com>
2021-04-16 16:23:22 +0800

    修复Cascader示例bug

--- components/Cascader/demos/value.js
+++ components/Cascader/demos/value.js
@@ -33,9 +33,9 @@ define(['./data.js'], function (options) {
                 unselectedText: '单值取值',
                 onValueChange({ newValue }) {
                   if (newValue) {
-                    cascade.update({ valueType: 'cascade' })
+                    cascade.update({ valueType: 'cascade', value: ['内分泌', '肥胖症', 'aaa'] })
                   } else {
-                    cascade.update({ valueType: 'single' })
+                    cascade.update({ valueType: 'single', value: 'aaa' })
                   }
                 },
               },
