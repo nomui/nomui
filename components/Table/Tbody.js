8de639e547ce9ee7d5b2a0d8aeacae0f810f760d
daniel.su <dan.su@ctsmed.com>
2021-04-28 11:16:12 +0800

    table empty调整

--- components/Table/Tbody.js
+++ components/Table/Tbody.js
@@ -38,7 +38,7 @@ class Tbody extends Component {
         children: {
           tag: 'Td',
           attrs: {
-            colspan: this.table.element.rows[0].childElementCount,
+            colspan: this.table.props.columns.length,
             style: {
               padding: '25px 0',
             },
