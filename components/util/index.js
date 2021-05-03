c2b722f026f1d2a94960a39fa4a94ee938af78a6
edison.yuan <1325225487@qq.com>
2021-05-04 00:44:06 +0800

    ts

--- components/util/index.js
+++ components/util/index.js
@@ -1,44 +1,44 @@
 String.prototype.trim = function (characters) {
-  return this.replace(new RegExp(`^${characters}+|${characters}+$`, 'g'), '')
+    return this.replace(new RegExp(`^${characters}+|${characters}+$`, 'g'), '')
 }
 
 String.prototype.startWith = function (str) {
-  const reg = new RegExp(`^${str}`)
-  return reg.test(this)
+    const reg = new RegExp(`^${str}`)
+    return reg.test(this)
 }
 
 String.prototype.trimEnd = function (characters) {
-  return this.replace(new RegExp(`${characters}+$`, 'g'), '')
+    return this.replace(new RegExp(`${characters}+$`, 'g'), '')
 }
 
 String.prototype.prepend = function (character) {
-  if (this[0] !== character) {
-    return (character + this).toString()
-  }
+    if (this[0] !== character) {
+        return (character + this).toString()
+    }
 
-  return this.toString()
+    return this.toString()
 }
 
 String.prototype.format = function (args) {
-  let result = this
-  if (arguments.length > 0) {
-    if (arguments.length === 1 && typeof args === 'object') {
-      for (const key in args) {
-        if (args[key] !== undefined) {
-          const reg = new RegExp(`({${key}})`, 'g')
-          result = result.replace(reg, args[key])
+    let result = this
+    if (arguments.length > 0) {
+        if (arguments.length === 1 && typeof args === 'object') {
+            for (const key in args) {
+                if (args[key] !== undefined) {
+                    const reg = new RegExp(`({${key}})`, 'g')
+                    result = result.replace(reg, args[key])
+                }
+            }
+        } else {
+            for (let i = 0; i < arguments.length; i++) {
+                if (arguments[i] !== undefined) {
+                    const reg = new RegExp(`({)${i}(})`, 'g')
+                    result = result.replace(reg, arguments[i])
+                }
+            }
         }
-      }
-    } else {
-      for (let i = 0; i < arguments.length; i++) {
-        if (arguments[i] !== undefined) {
-          const reg = new RegExp(`({)${i}(})`, 'g')
-          result = result.replace(reg, arguments[i])
-        }
-      }
     }
-  }
-  return result
+    return result
 }
 
 /**
@@ -52,21 +52,21 @@ String.prototype.format = function (args) {
 const { toString } = Object.prototype
 const OBJECT_STRING = '[object Object]'
 export function isPlainObject(obj) {
-  if (Object.prototype.toString.call(obj) !== OBJECT_STRING) {
-    return false
-  }
+    if (Object.prototype.toString.call(obj) !== OBJECT_STRING) {
+        return false
+    }
 
-  const prototype = Object.getPrototypeOf(obj)
-  return prototype === null || prototype === Object.prototype
+    const prototype = Object.getPrototypeOf(obj)
+    return prototype === null || prototype === Object.prototype
 }
 
 export function isString(obj) {
-  // 判断对象是否是字符串
-  return Object.prototype.toString.call(obj) === '[object String]'
+    // 判断对象是否是字符串
+    return Object.prototype.toString.call(obj) === '[object String]'
 }
 
 export function isFunction(val) {
-  return toString.call(val) === '[object Function]'
+    return toString.call(val) === '[object Function]'
 }
 
 /**
@@ -78,175 +78,175 @@ export function isFunction(val) {
 
 const hyphenateRE = /([^-])([A-Z])/g
 export function hyphenate(str) {
-  return str.replace(hyphenateRE, '$1-$2').replace(hyphenateRE, '$1-$2').toLowerCase()
+    return str.replace(hyphenateRE, '$1-$2').replace(hyphenateRE, '$1-$2').toLowerCase()
 }
 
 export function htmlEncode(value) {
-  // Create a in-memory element, set its inner text (which is automatically encoded)
-  // Then grab the encoded contents back out. The element never exists on the DOM.
-  const textarea = document.createElement('textarea')
-  textarea.textContent = value
-  return textarea.innerHTML
+    // Create a in-memory element, set its inner text (which is automatically encoded)
+    // Then grab the encoded contents back out. The element never exists on the DOM.
+    const textarea = document.createElement('textarea')
+    textarea.textContent = value
+    return textarea.innerHTML
 }
 
 export function extend() {
-  let options
-  let name
-  let src
-  let copy
-  let _clone
-  let target = arguments[0] || {}
-  let i = 1
-  const { length } = arguments
-  let deep = false
+    let options
+    let name
+    let src
+    let copy
+    let _clone
+    let target = arguments[0] || {}
+    let i = 1
+    const { length } = arguments
+    let deep = false
 
-  // Handle a deep copy situation
-  if (typeof target === 'boolean') {
-    deep = target
+    // Handle a deep copy situation
+    if (typeof target === 'boolean') {
+        deep = target
 
-    // Skip the boolean and the target
-    target = arguments[i] || {}
-    i++
-  }
-  // Handle case when target is a string or something (possible in deep copy)
-  if (typeof target !== 'object' && !isFunction(target)) {
-    target = {}
-  }
-
-  for (; i < length; i++) {
-    // Only deal with non-null/undefined values
-    if ((options = arguments[i]) != null) {
-      // Extend the base object
-      for (name in options) {
-        src = target[name]
-        copy = options[name]
-        // Prevent never-ending loop
-        if (target === copy) {
-          continue
-        }
-        // Recurse if we're merging plain objects
-        if (deep && copy && isPlainObject(copy)) {
-          _clone = src && isPlainObject(src) ? src : {}
-          // Never move original objects, clone them
-          target[name] = extend(deep, _clone, copy)
-          // Don't bring in undefined values
-        } else if (copy !== undefined) {
-          target[name] = copy
-        }
-      }
+        // Skip the boolean and the target
+        target = arguments[i] || {}
+        i++
     }
-  }
-  // Return the modified object
-  return target
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== 'object' && !isFunction(target)) {
+        target = {}
+    }
+
+    for (; i < length; i++) {
+        // Only deal with non-null/undefined values
+        if ((options = arguments[i]) != null) {
+            // Extend the base object
+            for (name in options) {
+                src = target[name]
+                copy = options[name]
+                // Prevent never-ending loop
+                if (target === copy) {
+                    continue
+                }
+                // Recurse if we're merging plain objects
+                if (deep && copy && isPlainObject(copy)) {
+                    _clone = src && isPlainObject(src) ? src : {}
+                    // Never move original objects, clone them
+                    target[name] = extend(deep, _clone, copy)
+                    // Don't bring in undefined values
+                } else if (copy !== undefined) {
+                    target[name] = copy
+                }
+            }
+        }
+    }
+    // Return the modified object
+    return target
 }
 
 export function clone(from) {
-  if (isPlainObject(from)) {
-    return JSON.parse(JSON.stringify(from))
-  }
+    if (isPlainObject(from)) {
+        return JSON.parse(JSON.stringify(from))
+    }
 
-  return from
+    return from
 }
 
 export function accessProp(options, key) {
-  if (typeof key === 'string') {
-    // Handle nested keys, e.g., "foo.bar" => { foo: { bar: ___ } }
-    const parts = key.split('.')
-    let curOption
-    key = parts.shift()
-    if (parts.length) {
-      curOption = options[key]
-      for (let i = 0; i < parts.length - 1; i++) {
-        curOption[parts[i]] = curOption[parts[i]] || {}
-        curOption = curOption[parts[i]]
-      }
-      key = parts.pop()
-      return curOption[key] === undefined ? null : curOption[key]
-    }
+    if (typeof key === 'string') {
+        // Handle nested keys, e.g., "foo.bar" => { foo: { bar: ___ } }
+        const parts = key.split('.')
+        let curOption
+        key = parts.shift()
+        if (parts.length) {
+            curOption = options[key]
+            for (let i = 0; i < parts.length - 1; i++) {
+                curOption[parts[i]] = curOption[parts[i]] || {}
+                curOption = curOption[parts[i]]
+            }
+            key = parts.pop()
+            return curOption[key] === undefined ? null : curOption[key]
+        }
 
-    return options[key] === undefined ? null : options[key]
-  }
+        return options[key] === undefined ? null : options[key]
+    }
 }
 
 export function pathCombine() {
-  let path = ''
-  const args = Array.from(arguments)
+    let path = ''
+    const args = Array.from(arguments)
 
-  args.forEach(function (item, index) {
-    if (index > 0) {
-      path += `/${item.trim('/')}`
-    } else {
-      path += item.trimEnd('/')
-    }
-  })
+    args.forEach(function (item, index) {
+        if (index > 0) {
+            path += `/${item.trim('/')}`
+        } else {
+            path += item.trimEnd('/')
+        }
+    })
 
-  return path
+    return path
 }
 
 const uppercaseRegex = /[A-Z]/g
 function toLowerCase(capital) {
-  return `-${capital.toLowerCase()}`
+    return `-${capital.toLowerCase()}`
 }
 export function normalizeKey(key) {
-  return key[0] === '-' && key[1] === '-'
-    ? key
-    : key === 'cssFloat'
-      ? 'float'
-      : key.replace(uppercaseRegex, toLowerCase)
+    return key[0] === '-' && key[1] === '-'
+        ? key
+        : key === 'cssFloat'
+            ? 'float'
+            : key.replace(uppercaseRegex, toLowerCase)
 }
 
 export function isNumeric(val) {
-  const num = Number(val)
-  const type = typeof val
-  return (
-    (val != null &&
-      type !== 'boolean' &&
-      (type !== 'string' || val.length) &&
-      !Number.isNaN(num) &&
-      Number.isFinite(num)) ||
-    false
-  )
+    const num = Number(val)
+    const type = typeof val
+    return (
+        (val != null &&
+            type !== 'boolean' &&
+            (type !== 'string' || val.length) &&
+            !Number.isNaN(num) &&
+            Number.isFinite(num)) ||
+        false
+    )
 }
 
 export function newGuid() {
-  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (n) {
-    // eslint-disable-next-line no-bitwise
-    const t = (Math.random() * 16) | 0
-    // eslint-disable-next-line no-bitwise
-    const i = n === 'x' ? t : (t & 3) | 8
-    return i.toString(16)
-  })
+    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (n) {
+        // eslint-disable-next-line no-bitwise
+        const t = (Math.random() * 16) | 0
+        // eslint-disable-next-line no-bitwise
+        const i = n === 'x' ? t : (t & 3) | 8
+        return i.toString(16)
+    })
 }
 
 export function isPromiseLike(promiseLike) {
-  return (
-    promiseLike !== null &&
-    (typeof promiseLike === 'object' || typeof promiseLike === 'function') &&
-    typeof promiseLike.then === 'function'
-  )
+    return (
+        promiseLike !== null &&
+        (typeof promiseLike === 'object' || typeof promiseLike === 'function') &&
+        typeof promiseLike.then === 'function'
+    )
 }
 
 export function formatDate(date, format) {
-  if (!date) {
-    return null
-  }
-  let mydate = null
-  if (typeof date === 'string') {
-    const arr = date
-      .replace(/\d+(?=-[^-]+$)/, function (a) {
-        return parseInt(a, 10) - 1
-      })
-      .match(/\d+/g)
+    if (!date) {
+        return null
+    }
+    let mydate = null
+    if (typeof date === 'string') {
+        const arr = date
+            .replace(/\d+(?=-[^-]+$)/, function (a) {
+                return parseInt(a, 10) - 1
+            })
+            .match(/\d+/g)
 
-    mydate = new Date(...arr)
-  } else if (typeof date === 'number') {
-    mydate = new Date(date)
-  }
+        mydate = new Date(...arr)
+    } else if (typeof date === 'number') {
+        mydate = new Date(date)
+    }
 
-  return new Date(mydate).format(format)
+    return new Date(mydate).format(format)
 }
 
 export default {
-  extend,
-  isFunction,
+    extend,
+    isFunction,
 }
