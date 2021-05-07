1462b31a956fc66883244face2d6098a130173c8
daniel.su <dan.su@ctsmed.com>
2021-05-07 10:14:29 +0800

    Cols 添加功能

--- components/Cols/demos/flex.js
+++ components/Cols/demos/flex.js
@@ -75,9 +75,42 @@ define([], function () {
                     component: 'Button',
                     text: 'item',
                   },
+                  {
+                    component: 'Button',
+                    text: 'item',
+                  },
                 ],
 
                 fills: true,
+              },
+            },
+          },
+          {
+            component: 'Panel',
+            header: {
+              caption: {
+                title: '拉伸其中一项',
+              },
+            },
+            body: {
+              children: {
+                component: 'Cols',
+                items: [
+                  {
+                    component: 'Button',
+                    text: 'item',
+                  },
+                  {
+                    component: 'Button',
+                    text: '我所在的容器会被拉伸，实现两端对齐',
+                  },
+                  {
+                    component: 'Button',
+                    text: 'item',
+                  },
+                ],
+
+                strechIndex: 1,
               },
             },
           },
