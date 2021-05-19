a63e233e7fffed9c81327d6540ac057d7bc6d7d3
jerry-nom <75375355+jerry-nom@users.noreply.github.com>
2021-05-19 07:25:51 +0000

    Deploying to gh-pages from @ nomui/nomui@6917fa7a3dfebfe3be99071d99a5c6b54d71b6df 🚀

---
+++ components/Anchor/index.md
@@ -0,0 +1,22 @@
+锚点跳转
+
+## 何时使用
+
+- 用于需要锚点跳转以及高亮的页面
+
+## API
+
+| 参数 | 说明 | 类型 | 默认值 |
+| --- | --- | --- | --- |
+| items | 菜单项，其中 key 指向对应内容的 key | `{text,key}` | `[]` |
+| border | 是否显示边框，以及边框位置 | `boolean` \| `left \| right` | `left` |
+| onItemClick | 点击锚点回调 | `(args)=>{}` | - |
+| width | 锚点菜单宽度 | `number` | `180` |
+| sticky | 是否固定锚点菜单，为`true`时以`window`作为滚动容器，否则传入锚点依附的滚动容器，false 的时候锚点菜单不固定 | `object \| function \| boolean` | - |
+| container | 锚点内容依附的滚动容器 | `object` | - |
+
+### AnchorContent 锚点内容组件
+
+| 参数 | 说明               | 类型     | 默认值 |
+| ---- | ------------------ | -------- | ------ |
+| key  | 对应锚点菜单的 key | `string` | -      |
--- dist/nomui.js
+++ dist/nomui.js
@@ -2703,6 +2703,132 @@ function _defineProperty2(obj, key, value) {
     }
   }
   Component.register(Alert);
+  class Anchor extends Component {
+    constructor(props, ...mixins) {
+      const defaults = {
+        container: null,
+        items: [],
+        border: "left",
+        onItemClick: null,
+        width: 180,
+        sticky: false,
+        itemDefaults: null,
+      };
+      super(Component.extendProps(defaults, props), ...mixins);
+    }
+    _created() {
+      this.container = isFunction(this.props.container)
+        ? this.props.container()
+        : this.props.container;
+    }
+    _config() {
+      const that = this;
+      const { items, border, width, itemDefaults } = this.props;
+      const myWidth = isNumeric(width) ? `${width}px` : width;
+      this.setProps({
+        classes: {
+          "nom-anchor-border-left": border === "left",
+          "nom-anchor-border-right": border === "right",
+        },
+        attrs: { style: { "min-width": myWidth } },
+        children: {
+          component: "Menu",
+          ref: (c) => {
+            that.menu = c;
+          },
+          itemSelectable: { byClick: true },
+          items: items,
+          itemDefaults: Object.assign({}, itemDefaults, {
+            onClick: function (args) {
+              const key = args.sender.key;
+              that.props.onItemClick &&
+                that._callHandler(that.props.onItemClick, key);
+              that._scrollToKey(key);
+            },
+          }),
+        },
+      });
+    }
+    _rendered() {
+      const that = this;
+      this.position = null;
+      this.size = null;
+      if (this.props.sticky) {
+        if (this.props.sticky === true) {
+          this.scrollParent = window;
+          this.scrollParent.onscroll = function () {
+            that._fixPosition();
+          };
+        } else {
+          if (isFunction(this.props.sticky)) {
+            this.scrollParent = this.props.sticky();
+          } else {
+            this.scrollParent = this.props.sticky;
+          }
+          this.scrollParent._on("scroll", function () {
+            that._fixPosition();
+          });
+        }
+      }
+      this.container._on("scroll", function () {
+        that._onContainerScroll();
+      });
+    }
+    _scrollToKey(target) {
+      const container = this.container.element.getElementsByClassName(
+        `nom-anchor-target-${target}`
+      );
+      container.length && container[0].scrollIntoView({ behavior: "smooth" });
+    }
+    _fixPosition() {
+      this.element.style.transform = `translateY(0px)`;
+      let pRect = null;
+      if (this.props.sticky === true) {
+        pRect = { top: 0, height: window.innerHeight };
+      } else {
+        pRect = this.scrollParent.element.getBoundingClientRect();
+      }
+      const gRect = this.element.getBoundingClientRect();
+      if (gRect.top < pRect.top) {
+        this.element.style.transform = `translateY(${
+          pRect.top - gRect.top - 2
+        }px)`;
+      }
+    }
+    _onContainerScroll() {
+      const list = this.container.element.getElementsByClassName(
+        "nom-anchor-content"
+      );
+      const pRect = this.container.element.getBoundingClientRect();
+      let current = 0;
+      for (let i = 0; i < list.length; i++) {
+        const top = list[i].getBoundingClientRect().top;
+        const lastTop = i > 0 ? list[i - 1].getBoundingClientRect().top : 0;
+        if (top < pRect.bottom && lastTop < pRect.top) {
+          current = i;
+        }
+      }
+      const classes = list[current].classList.value;
+      const idx = classes.indexOf("target-");
+      const result = classes.slice(idx + 7);
+      this._activeAnchor(result);
+    }
+    _activeAnchor(key) {
+      this.menu.selectItem(key, { scrollIntoView: false });
+    }
+  }
+  Component.register(Anchor);
+  class AnchorContent extends Component {
+    constructor(props, ...mixins) {
+      const defaults = { key: null };
+      super(Component.extendProps(defaults, props), ...mixins);
+    }
+    _rendered() {
+      const { key } = this.props;
+      this.element.classList.add(`nom-anchor-target-${key}`);
+    }
+  }
+  Component.register(AnchorContent);
   class Route {
     constructor(defaultPath) {
       const that = this;
@@ -14059,7 +14185,7 @@ function _defineProperty2(obj, key, value) {
         return false;
       }
       item.select(selectOption);
-      this.scrollTo(item);
+      selectOption && selectOption.scrollIntoView && this.scrollTo(item);
       return item;
     }
     selectToItem(param) {
@@ -20445,6 +20571,8 @@ function _defineProperty2(obj, key, value) {
     plugin.install(this);
   }
   exports.Alert = Alert;
+  exports.Anchor = Anchor;
+  exports.AnchorContent = AnchorContent;
   exports.App = App;
   exports.AutoComplete = AutoComplete;
   exports.Avatar = Avatar;
