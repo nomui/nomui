052b24b3c2262f805051bf60e211ed50cbb44e6f
daniel.su <dan.su@ctsmed.com>
2021-05-19 15:22:17 +0800

    demo改用Anchor部分组件案例需要修改

--- components/Grid/demos/sticky.js
+++ components/Grid/demos/sticky.js
@@ -10,7 +10,7 @@ define([], function () {
         bordered: true,
         frozenHeader: true,
         frozenLeftCols: 1,
-        sticky: this.parent, // 指定有滚动条的父容器，自动开启吸附表头跟吸附滚动条，设为true则指定window为滚动容器
+        sticky: this.parent.parent.parent, // 指定有滚动条的父容器，自动开启吸附表头跟吸附滚动条，设为true则指定window为滚动容器
         columns: [
           {
             field: 'name',
