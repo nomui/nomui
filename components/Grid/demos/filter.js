47016c2861d698da629e5cffbc2a64bd2476a968
daniel.su <dan.su@ctsmed.com>
2021-05-26 11:24:10 +0800

    grid 列头图标间距以及颜色调整

--- components/Grid/demos/filter.js
+++ components/Grid/demos/filter.js
@@ -19,6 +19,7 @@ define([], function () {
                 component: 'Textbox',
               }
             },
+            sortable: true,
           },
           {
             field: 'author',
