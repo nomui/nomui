6ed9aa8e16d00c1738e37a26346efa01cc539e00
Jerry <jerry.zhou@wetrial.com>
2021-05-26 11:15:52 +0800

    Select 增加可搜索行为

--- components/List/List.js
+++ components/List/List.js
@@ -22,12 +22,10 @@ class List extends Component {
     super(Component.extendProps(defaults, props), ...mixins)
   }
 
-  _created() {
+  _config() {
     this.itemRefs = {}
     this.selectedItem = null
-  }
 
-  _config() {
     this._addPropStyle('gutter', 'line', 'align', 'justify', 'cols')
 
     this.setProps({
