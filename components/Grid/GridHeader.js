dffe8ade00b324f2380d0925041c5c2712240aa6
daniel.su <dan.su@ctsmed.com>
2021-05-31 16:53:52 +0800

    Grid sticky scroller bug fixed

--- components/Grid/GridHeader.js
+++ components/Grid/GridHeader.js
@@ -43,6 +43,7 @@ class GridHeader extends Component {
         target: this.grid,
       })
     }
+    this._hideScrolls()
     this.position = null
     this.size = null
 
@@ -66,6 +67,13 @@ class GridHeader extends Component {
 
   _remove() {
     this.scrollbar && this.scrollbar._remove()
+  }
+
+  _hideScrolls() {
+    const scrolls = document.getElementsByClassName('nom-scrollbar')
+    for (let i = 0; i < scrolls.length; i++) {
+      scrolls[i].classList.add('s-hidden')
+    }
   }
 
   _onPageScroll() {
