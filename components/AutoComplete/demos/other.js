2202ec2b410db274391650b4c9eda25052b4b77d
luoquan <quan.luo@wetrial.com>
2021-06-02 10:14:52 +0800

    修复autoComplete bug

--- components/AutoComplete/demos/other.js
+++ components/AutoComplete/demos/other.js
@@ -1,43 +1,23 @@
 define([], function () {
   return {
-    title: '其他',
-    file: 'other',
+    title: '基础用法',
+    file: 'basic',
     demo: function () {
-      let autoComplete = null
-      let loading = null
-      let timer = null
+      const myPromise = (text) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(Array.from({ length: 5 }, (_, i) => ({ value: text.repeat(i + 1) })))
+          }, 3000)
+        })
       return {
         component: 'AutoComplete',
-        ref: (c) => {
-          autoComplete = c
-        },
-        label: '其他',
-        options: [
-          { value: 'a' },
-          { value: '我们的世界' },
-          { value: 'aab' },
-          { value: 'aac' },
-          { value: 'aadd' },
-          { value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
-        ],
-        placeholder: '请输入',
-        onSearch(txt) {
-          loading = new nomui.Loading({ container: autoComplete.input })
-          timer && clearTimeout(timer)
-          timer = setTimeout(() => {
-            loading && loading.remove()
-            if (txt) {
-              autoComplete.update({
-                options: [
-                  { value: txt },
-                  { value: txt.repeat(2) },
-                  { value: txt.repeat(3) },
-                  { value: txt.repeat(4) },
-                  { value: txt.repeat(5) },
-                ],
-              })
-            }
-          }, 1000)
+        debounce: true,
+        interval: 600,
+        options: [{ value: 'a' }, { value: 'aa' }, { value: 'ab' }],
+        onSearch({ sender, text }) {
+          myPromise(text).then((options) => {
+            sender.update({ options })
+          })
         },
       }
     },
