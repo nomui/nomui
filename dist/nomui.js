fbdb279c4c2a3f1ca61a324a613f008abdc43beb
jerry-nom <75375355+jerry-nom@users.noreply.github.com>
2021-06-10 02:41:08 +0000

    Deploying to gh-pages from @ nomui/nomui@678aaf161a15248399e76f2c081f77271888132d 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -13227,6 +13227,75 @@ function _defineProperty2(obj, key, value) {
     },
   });
   Component.register(Ellipsis);
+  class FlexItem extends Component {
+    constructor(props, ...mixins) {
+      const defaults = { grow: false, shrink: false, isBody: false };
+      super(Component.extendProps(defaults, props), ...mixins);
+    }
+    _config() {
+      this._propStyleClasses = ["grow", "shrink", "isBody"];
+    }
+  }
+  Component.register(FlexItem);
+  class Flex extends Component {
+    constructor(props, ...mixins) {
+      const defaults = {
+        rows: null,
+        cols: null,
+        direction: "column",
+        wrap: false,
+        align: null,
+        justify: null,
+        gap: null,
+        gutter: null,
+        fills: false,
+        inline: false,
+        fit: false,
+      };
+      super(Component.extendProps(defaults, props), ...mixins);
+    }
+    _config() {
+      this._propStyleClasses = [
+        "direction",
+        "wrap",
+        "align",
+        "justify",
+        "gap",
+        "gutter",
+        "fills",
+        "fit",
+      ];
+      const { rows, cols, itemDefaults } = this.props;
+      let { direction } = this.props;
+      let children = rows;
+      if (Array.isArray(cols) && cols.length) {
+        direction = "row";
+        children = cols;
+      }
+      const childDefaults = Component.extendProps(itemDefaults, {
+        component: FlexItem,
+        _config: (flexItem) => {
+          const { children: subChildren } = flexItem.props;
+          if (isPlainObject(subChildren)) {
+            const { rows: subRows, cols: subCols, component } = subChildren;
+            if (
+              !component &&
+              (Array.isArray(subRows) || Array.isArray(subCols))
+            ) {
+              subChildren.component = Flex;
+            }
+            flexItem.props.children = subChildren;
+          }
+        },
+      });
+      this.setProps({
+        direction: direction,
+        children: children,
+        childDefaults: childDefaults,
+      });
+    }
+  }
+  Component.register(Flex);
   class Form extends Group {
     constructor(props, ...mixins) {
       const defaults = { labelAlign: "top" };
@@ -21969,6 +22038,7 @@ function _defineProperty2(obj, key, value) {
   exports.Ellipsis = Ellipsis;
   exports.Empty = Empty;
   exports.Field = Field;
+  exports.Flex = Flex;
   exports.Form = Form;
   exports.Grid = Grid;
   exports.Group = Group;
