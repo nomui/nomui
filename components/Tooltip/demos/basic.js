0a7cad9be91016732c51d080b4bae01140472264
daniel.su <dan.su@ctsmed.com>
2021-06-23 15:12:59 +0800

    Tooltip用在按钮上时，点下按钮，mouseleave提示不消失

--- components/Tooltip/demos/basic.js
+++ components/Tooltip/demos/basic.js
@@ -1,15 +1,14 @@
 define([], function () {
+  return {
+    title: '基本用法',
+    file: 'basic',
+    demo: function () {
+      return {
+        component: 'Button',
 
-    return {
-        title: '基本用法',
-        file: 'basic',
-        demo: function () {
-            return {
-                tag: 'span',
-                children: '鼠标放上面会出来文字提示',
-                tooltip: '我是文字提示'
-            }
-        }
-    }
-
+        text: '鼠标放上面会出来文字提示',
+        tooltip: '我是文字提示',
+      }
+    },
+  }
 })
