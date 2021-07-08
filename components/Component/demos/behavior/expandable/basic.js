e8919585253dec63188bbcefb5d2eddd09e2f9f6
Jerry <jerry.zhou@wetrial.com>
2021-07-08 16:36:10 +0800

    修改 expandable 基本示例

--- components/Component/demos/behavior/expandable/basic.js
+++ components/Component/demos/behavior/expandable/basic.js
@@ -2,7 +2,8 @@ define([], function () {
   return {
     title: '基本用法',
     file: 'basic',
-    description: '通过 `expandable` 配置组件的可展开行为',
+    description:
+      '通过 `expandable` 配置组件的可展开行为。通过点击或鼠标悬浮可以控制 `expandable.target` 指定的目标组件的显示和隐藏。',
     demo: function () {
       let contentRef = null
       return {
