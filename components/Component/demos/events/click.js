b4dfb0408b7b71288cb12f5e839bfabc47160683
jerry-nom <75375355+jerry-nom@users.noreply.github.com>
2021-07-16 06:29:22 +0000

    Deploying to gh-pages from @ nomui/nomui@906a000a23219837125c30324a4bc6ce94ed36f6 🚀

--- components/Component/demos/events/click.js
+++ components/Component/demos/events/click.js
@@ -1,6 +1,6 @@
 define([], function () {
   return {
-    title: '点击事件',
+    title: 'onClick',
     file: 'click',
     description:
       '通过 `onClick` 响应组件的点击事件。注意与 `attrs.onclick` 配置的区别。组件的事件监听是直接配置在组件的第一个层级里的，且命名规范为 `onXxx`。按照规范定义的组件事件回调参数为一个对象，参数对象的 `sender` 属性代表组件自身实例，如果事件与 html 元素事件对应，则一般还会传递一个 `event` 属性，代表原生的事件参数。打开开发者工具查看打印的参数信息。',
