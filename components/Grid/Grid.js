98d3189eec79b5aeec0e96c3a1eb81a4cd18b48d
HuZhangjie <1534634433@qq.com>
2021-08-02 16:37:18 +0800

    fix(Grid): 自定义配置列时只存col的field属性

--- components/Grid/Grid.js
+++ components/Grid/Grid.js
@@ -34,11 +34,16 @@ class Grid extends Component {
     if (selected && selected.length) {
       this.props.visibleColumns = selected
     }
-    // 缓存中有数据则读取缓存中的cols数据
+    // 缓存中有数据则读取缓存中的col的field数据
     if (cacheKey) {
-      const storeCols = localStorage.getItem(`${STORAGE_KEY_GRID_COLUMNS}_${cacheKey}`)
-      if (storeCols) {
-        this.props.visibleColumns = JSON.parse(storeCols)
+      let storeFields = localStorage.getItem(`${STORAGE_KEY_GRID_COLUMNS}_${cacheKey}`)
+      if (storeFields && storeFields.length) {
+        storeFields = JSON.parse(storeFields)
+        // this.props.visibleColumns = JSON.parse(storeFields)
+        this.props.visibleColumns = this.originColumns.filter((item) =>
+          storeFields.includes(item.field),
+        )
+        console.log('🚀 ~ file: .props.visibleColumns', this.props.visibleColumns)
       }
     }
   }
@@ -173,7 +178,7 @@ class Grid extends Component {
     })
   }
 
-  getMappedColumns() {
+  getMappedColumns(columns) {
     const arr = []
     function mapColumns(data) {
       data.forEach(function (item) {
@@ -183,7 +188,7 @@ class Grid extends Component {
         arr.push(item.field)
       })
     }
-    mapColumns(this.originColumns)
+    mapColumns(columns || this.originColumns)
 
     return arr
   }
@@ -441,7 +446,10 @@ class Grid extends Component {
     const { columnsCustomizable } = this.props
     const { cache: cacheKey } = columnsCustomizable
     if (cacheKey) {
-      localStorage.setItem(`${STORAGE_KEY_GRID_COLUMNS}_${cacheKey}`, JSON.stringify(tree))
+      localStorage.setItem(
+        `${STORAGE_KEY_GRID_COLUMNS}_${cacheKey}`,
+        JSON.stringify(this.getMappedColumns(tree)),
+      )
     }
 
     columnsCustomizable.callback && this._callHandler(columnsCustomizable.callback(tree))
