42fb14547504bf5faaf390f455cfebd874d8e633
HuZhangjie <1534634433@qq.com>
2021-10-11 13:58:42 +0800

    fix(Grid-Td): 修改了Td的层级结构，导致设置ellipsis时的title无效了

--- components/Table/Td.js
+++ components/Table/Td.js
@@ -155,10 +155,7 @@ class Td extends Component {
         colspan: colSpan,
         rowspan: rowSpan,
         'data-field': this.props.column.field,
-        title:
-          (isString(children) || isNumeric(children)) && (isEllipsis || showTitle)
-            ? children
-            : null,
+        title: this._getAttrTitle(children, isEllipsis, showTitle)
       },
       hidden: colSpan === 0 || rowSpan === 0,
       classes: {
@@ -171,6 +168,17 @@ class Td extends Component {
         'nom-table-ellipsis': isEllipsis,
       },
     })
+  }
+
+  _getAttrTitle(children, isEllipsis, showTitle) {
+    // 因为isEllipsis = true时，已经使用span包了一层，所以具体的title为children.children
+    if(isEllipsis || showTitle) {
+      const _title = isEllipsis ? children.children : children
+      if(isString(_title) || isNumeric(_title)) {
+        return _title
+      }
+    }
+    return null
   }
 
   _rendered() {
