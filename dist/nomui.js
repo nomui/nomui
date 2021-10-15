ed6bc7ad8815468940d62746ba9985168d90fdc2
fatalisa <40448732+fatalisa@users.noreply.github.com>
2021-10-15 01:32:45 +0000

    Deploying to gh-pages from @ nomui/nomui@f5f67b86093a6a871aef24d3c59d896b9c0424b4 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -15285,6 +15285,7 @@ function _defineProperty2(obj, key, value) {
           onscroll: () => {
             const { scrollLeft } = this.element;
             this.grid.header.element.scrollLeft = scrollLeft;
+            this.grid.header.scrollbar.setScrollLeft(scrollLeft);
           },
         },
       });
@@ -15325,7 +15326,11 @@ function _defineProperty2(obj, key, value) {
           },
           onscroll: () => {
             const { scrollLeft } = this.element;
-            this.props.target.body.element.scrollLeft = scrollLeft;
+            this._scrollLeft = scrollLeft;
+            if (!this._setScrollFlag) {
+              this.props.target.body.element.scrollLeft = scrollLeft;
+            }
+            this._setScrollFlag = false;
           },
         },
         children: {
@@ -15333,6 +15338,21 @@ function _defineProperty2(obj, key, value) {
           attrs: { style: { width: size.innerWidth } },
         },
       });
+    } // 外部调用方法设置 scrollBar 的 scrollLeft
+    setScrollLeft(scrollLeft) {
+      // 当前scrollbar 隐藏，无法直接设置scrollLeft 的值
+      // 所以先记录下来 在_rendered中再赋值
+      if (this.props.hidden) {
+        this._scrollLeft = scrollLeft;
+      } else {
+        // _setScrollFlag: true 时：表示此次滚动条的变化为外部触发
+        // 则无需再正向设置 gridbody scrollLeft, 导致触发其 onscroll事件
+        this._setScrollFlag = true;
+        this.element.scrollLeft = scrollLeft;
+      }
+    }
+    _rendered() {
+      this.element.scrollLeft = this._scrollLeft || 0;
     }
     show() {
       this.props.hidden && this.update({ hidden: false });
