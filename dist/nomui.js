8ffd5c2714ce4be9b3dbf478b97cfec0ff73cd3b
fatalisa <40448732+fatalisa@users.noreply.github.com>
2021-11-19 09:09:38 +0000

    Deploying to gh-pages from @ nomui/nomui@3170937fc839e796d99b46b9d4fffff82d723981 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -25,7 +25,7 @@ function _defineProperty2(obj, key, value) {
 }
 /**
  *
- *       nomui v1.0.0-alpha.36
+ *       nomui v1.0.0-alpha.37
  *       License: MIT
  *       Copyright (c) 2021-2021, Wetrial
  *
