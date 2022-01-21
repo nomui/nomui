168868f9220a371c501d0243ad1213f654de6393
jerry-nom <75375355+jerry-nom@users.noreply.github.com>
2022-01-21 03:54:15 +0000

    Deploying to gh-pages from @ nomui/nomui@0380b7d3324a66b3bcf8b051349accfdbc7e2ef4 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -18991,30 +18991,44 @@ function _defineProperty2(obj, key, value) {
         min: null,
         max: null,
         precision: -1,
+        maxPrecision: null,
+        limitInput: false,
         allowClear: false,
       };
       super(Component.extendProps(defaults, props), ...mixins);
     }
     _config() {
-      const { precision = -1 } = this.props;
+      let { precision, maxPrecision } = this.props;
+      const { limitInput } = this.props;
+      if (limitInput) {
+        maxPrecision = null;
+      }
+      if (maxPrecision) {
+        precision = -1;
+        this.rules.push({
+          type: "regex",
+          value: { pattern: `^\\d+(\\.\\d{1,${maxPrecision}})?$` },
+          message: `请输入有效数字，且最多包含${maxPrecision}位小数`,
+        });
+      }
       if (precision === -1) {
         this.rules.push({ type: "number" });
       } // 允许输入千分位加 , 的格式的数字
-      if (this.props.precision === 0) {
+      if (precision === 0) {
         this.rules.push({
           type: "regex",
           value: { pattern: /^-?(\d+|\d{1,3}(,\d{3})+)$/ },
           message: "请输入有效整数",
         });
       }
-      if (this.props.precision > 0) {
+      if (precision > 0) {
         this.rules.push({
           type: "regex",
           value: {
             // 在上面的规则的基础上添加了小数部分
-            pattern: `^\\-?(\\d+|\\d{1,3}(,\\d{3})+)(\\.\\d{${this.props.precision}})$`,
+            pattern: `^\\-?(\\d+|\\d{1,3}(,\\d{3})+)(\\.\\d{${precision}})$`,
           },
-          message: `请输入有效 ${this.props.precision} 位小数`,
+          message: `请输入有效数字，且包含 ${precision} 位小数`,
         });
       }
       if (this.props.min) {
@@ -19024,6 +19038,28 @@ function _defineProperty2(obj, key, value) {
         this.rules.push({ type: "max", value: this.props.max });
       }
       super._config();
+    }
+    _onBlur() {
+      if (!this.props.limitInput || this.props.precision < 0) {
+        return;
+      }
+      this._toFixedValue();
+    }
+    _toFixedValue() {
+      const { precision } = this.props;
+      let r = "";
+      const c = this.input.getText().replace(/[^0-9,.]/gi, "");
+      const i = c.indexOf(".");
+      r = i > -1 ? c.substring(0, i) : c;
+      if (precision > 0) {
+        let dec = i > -1 ? parseFloat(c.substring(i)) : parseFloat(0);
+        if (!isNumeric(dec)) {
+          return;
+        }
+        dec = dec.toFixed(precision);
+        r += dec.substring(1);
+      }
+      this.input.setText(r);
     }
     _getValue() {
       const { precision = -1 } = this.props;
