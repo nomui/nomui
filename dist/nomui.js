6b598c57bc195324e8b64aa10f8e3c23f7f55f89
fatalisa <40448732+fatalisa@users.noreply.github.com>
2022-01-26 07:38:07 +0000

    Deploying to gh-pages from @ nomui/nomui@a349ea78b0bc891bffc96837e1ae7aa8679a7bad 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -38,7 +38,73 @@ function _defineProperty2(obj, key, value) {
         typeof globalThis !== "undefined" ? globalThis : global || self),
       factory((global.nomui = {})));
 })(this, function (exports) {
-  "use strict";
+  "use strict"; // 初始化时存下浏览器的版本信息
+  const BROWSER_INFO = getBrowser();
+  /**
+   * 获取浏览器的版本信息
+   * @returns {type: string, version: number}
+   */ function getBrowser() {
+    const UserAgent = navigator.userAgent.toLowerCase();
+    const browserInfo = {};
+    const browserArray = {
+      IE: window.ActiveXObject || "ActiveXObject" in window, // IE
+      Chrome:
+        UserAgent.indexOf("chrome") > -1 && UserAgent.indexOf("safari") > -1, // Chrome浏览器
+      Firefox: UserAgent.indexOf("firefox") > -1, // 火狐浏览器
+      Opera: UserAgent.indexOf("opera") > -1, // Opera浏览器
+      Safari:
+        UserAgent.indexOf("safari") > -1 && UserAgent.indexOf("chrome") === -1, // safari浏览器
+      Edge: UserAgent.indexOf("edge") > -1, // Edge浏览器
+      QQBrowser: /qqbrowser/.test(UserAgent), // qq浏览器
+      WeixinBrowser: /MicroMessenger/i.test(UserAgent), // 微信浏览器
+    };
+    for (const i in browserArray) {
+      if (browserArray[i]) {
+        let version = "";
+        if (i === "IE") {
+          version = UserAgent.match(/(msie\s|trident.*rv:)([\w.]+)/)[2];
+        } else if (i === "Chrome") {
+          version = UserAgent.match(/chrome\/([\d.]+)/)[1];
+        } else if (i === "Firefox") {
+          version = UserAgent.match(/firefox\/([\d.]+)/)[1];
+        } else if (i === "Opera") {
+          version = UserAgent.match(/opera\/([\d.]+)/)[1];
+        } else if (i === "Safari") {
+          version = UserAgent.match(/version\/([\d.]+)/)[1];
+        } else if (i === "Edge") {
+          version = UserAgent.match(/edge\/([\d.]+)/)[1];
+        } else if (i === "QQBrowser") {
+          version = UserAgent.match(/qqbrowser\/([\d.]+)/)[1];
+        }
+        browserInfo.type = i;
+        browserInfo.version = parseInt(version, 10);
+      }
+    }
+    return browserInfo;
+  }
+  console.log(getBrowser()); // 支持 position: sticky 属性的最小版本
+  // 参考 https://caniuse.com/?search=sticky
+  const SUPPORT_STICKY_MIN_BROWSER_VERSION_MAP = {
+    Chrome: 56,
+    Firefox: 59,
+    Opera: 42,
+    Safari: 6,
+    Edge: 16,
+    QQBrowser: 10,
+  }; // 浏览器是否支持sticky
+  function isBrowerSupportSticky() {
+    if (BROWSER_INFO.type === "IE") return false; //
+    // 低于 minVersion 版本的浏览器，都不支持
+    const minVersion =
+      SUPPORT_STICKY_MIN_BROWSER_VERSION_MAP[BROWSER_INFO.type];
+    if (minVersion && minVersion > BROWSER_INFO.version) {
+      return false;
+    } // 未知的浏览器版本都默认支持
+    return true;
+  }
+  function isChrome49() {
+    return BROWSER_INFO.type === "Chrome" && BROWSER_INFO.version === 49;
+  }
   String.prototype.trim = function (characters) {
     return this.replace(new RegExp(`^${characters}+|${characters}+$`, "g"), "");
   };
@@ -352,6 +418,8 @@ function _defineProperty2(obj, key, value) {
     debounce: debounce,
     isNotEmptyArray: isNotEmptyArray,
     getStyle: getStyle,
+    isBrowerSupportSticky: isBrowerSupportSticky,
+    isChrome49: isChrome49,
   }); // Events
   // -----------------
   // Thanks to:
@@ -15862,6 +15930,12 @@ function _defineProperty2(obj, key, value) {
         },
       });
     }
+    _rendered() {
+      // fix: chrome下,最下面的横向滚动条会挡住部分内容,读取过一次dom的属性后,又恢复正常
+      if (isChrome49()) {
+        this._elWidth = this.element.offsetWidth;
+      }
+    }
   }
   Component.register(GridBody);
   class GridFooter extends Component {
@@ -16265,7 +16339,8 @@ function _defineProperty2(obj, key, value) {
     }
     _config() {
       this.nodeList = {};
-      const that = this; // 切换分页 data数据更新时 此两项不重置会导致check表现出错
+      const that = this;
+      this._parseBrowerVersion(); // 切换分页 data数据更新时 此两项不重置会导致check表现出错
       this.rowsRefs = {};
       this.checkedRowRefs = {};
       this._propStyleClasses = ["bordered"];
@@ -16366,6 +16441,14 @@ function _defineProperty2(obj, key, value) {
         }
       });
       return treeData;
+    }
+    _parseBrowerVersion() {
+      // 不支持sticky，需要将frozen 置为null
+      if (!isBrowerSupportSticky()) {
+        this.props.frozenLeftCols = null;
+        this.props.frozenRightCols = null;
+        this.props.allowFrozenCols = false;
+      }
     }
     _parseColumnsCustom() {
       const { columnsCustomizable, visibleColumns } = this.props; // 未设置自定义列展示
