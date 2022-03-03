351cba05b9768d7a197204a0893a9927339c3d59
daniel.su <dan.su@ctsmed.com>
2022-03-03 11:37:43 +0800

    chore: 修改stylelint-order配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/stylelint')],
   rules: {
-    'order/properties-order': null,
+    'order/properties-order': [],
     'declaration-bang-space-before': null,
     'function-name-case': null,
     'plugin/declaration-block-no-ignored-properties': null,
