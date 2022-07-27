1cf8ce7d52b99d72df0d51d34e9995c48524ccab
jieWangDevil <jie.wang@wetrial.com>
2022-07-27 09:55:51 +0800

    feat: Uploader支持自定义t头像上传按钮

--- components/Uploader/Uploader.js
+++ components/Uploader/Uploader.js
@@ -9,7 +9,7 @@ import {
   getUUID,
   isBlobFile,
   isPromiseLike,
-  removeFile,
+  removeFile
 } from './helper'
 import Request from './request'
 
@@ -24,7 +24,7 @@ class Uploader extends Field {
   _created() {
     // this.fileList = this.props.fileList || this.props.defaultFileList
     this._updateFile = null
-
+    this._updateFileIcon = []
     super._created()
   }
 
@@ -42,10 +42,13 @@ class Uploader extends Field {
       onRemove,
       renderer,
       customizeInfo,
+      type
     } = this.props
 
     this.fileList = this.props.fileList || this.props.defaultFileList
-
+    if (this.fileList && this.fileList.length > 0) {
+      this.fileList = type === 'normal' ? this.fileList : this.fileList.slice(-1)
+    }
     this.acceptList = accept ? this.getAcceptList() : ''
 
     let initializing = true
@@ -111,32 +114,70 @@ class Uploader extends Field {
       button = Component.extendProps(defaults, button)
       children.push(button)
     }
-
-    if (display) {
-      if (initializing || (this.fileList && this.fileList.length > 0)) {
-        children.push({
-          component: FileList,
-          classes: {
-            'nom-file-list-only': button === false,
-          },
-          ref: (c) => {
-            that.list = c
-          },
-          initializing,
-          files: display === 'replace' && !multiple ? this.fileList.slice(-1) : this.fileList,
-          renderer,
-          onRemove: onRemove &&
-            isFunction(onRemove.action) && {
+    if (type === 'normal') {
+      if (display) {
+        if (initializing || (this.fileList && this.fileList.length > 0)) {
+          children.push({
+            component: FileList,
+            classes: {
+              'nom-file-list-only': button === false,
+            },
+            ref: (c) => {
+              that.list = c
+            },
+            initializing,
+            files: display === 'replace' && !multiple ? this.fileList.slice(-1) : this.fileList,
+            renderer,
+            onRemove: onRemove &&
+              isFunction(onRemove.action) && {
               ...onRemove,
               action: that.handleRemove.bind(that),
             },
-          allowUpdate,
-          extraAction,
-          customizeInfo,
+            allowUpdate,
+            extraAction,
+            customizeInfo,
+          })
+        }
+      }
+    } else if (this.fileList && this.fileList.length) {
+      if (this.fileList[0].status === 'uploading' && !this._updateFileIcon.includes('loading')) {
+        button.children.push({
+          component: 'Icon',
+          type: 'loading',
+          classes: {
+            'file-loading-img': true,
+          },
+        })
+        this._updateFileIcon.push('loading')
+      } else if (this.fileList[0].status === 'done' && !this._updateFileIcon.includes('close-circle')) {
+        button.children.push({
+          component: 'Icon',
+          type: 'close-circle',
+          classes: {
+            'close-circle-btn': true
+          },
+          onClick: ({ event }) => {
+            event.stopPropagation()
+            this.handleRemove({ sender: that, file: this.fileList[0] })
+            onRemove.action({ sender: that, file: this.fileList[0] })
+          }
+        })
+        this._updateFileIcon.push('close-circle')
+        this._updateFileIcon.splice(this._updateFileIcon.indexOf("error"), 1);
+        this.deleteIcon('loading', button)
+      } else if (this.fileList[0].status === 'error' && !this._updateFileIcon.includes('error')) {
+        this.deleteIcon('loading', button)
+        new nomui.Message({
+          content: '上传失败！',
+          type: 'error',
         })
       }
+      if (this.fileList[0].status !== 'uploading') {
+        this._updateFileIcon.splice(this._updateFileIcon.indexOf("loading"), 1);
+      }
+    } else {
+      this.deleteIcon('close-circle', button)
     }
-
     this.setProps({
       control: {
         children,
@@ -144,6 +185,12 @@ class Uploader extends Field {
     })
 
     super._config()
+  }
+
+  deleteIcon(name, file) {
+    this._updateFileIcon.splice(this._updateFileIcon.indexOf(name), 1);
+    const index = file.children.findIndex(element => element.type === name)
+    if (index > 0) file.children.splice(index, 1)
   }
 
   getAcceptList() {
@@ -281,7 +328,6 @@ class Uploader extends Field {
   onChange(info) {
     // 更新列表
     this.fileList = info.fileList
-
     const { onChange: onChangeProp } = this.props
     this.update({ fileList: [...info.fileList] })
 
@@ -385,9 +431,8 @@ class Uploader extends Field {
     // removing
     file.status = 'removing'
     this.fileList = this.fileList.map((f) =>
-      f.uuid === file.uuid ? { ...f, status: 'removing' } : f,
+      f.uuid === file.uuid ? { ...f, status: 'removing' } : f
     )
-
     this.onChange({
       file,
       fileList: this.fileList,
@@ -473,6 +518,7 @@ Uploader.defaults = {
   renderer: null,
   extraAction: [],
   customizeInfo: null,
+  type: 'normal',
 }
 
 Component.register(Uploader)
---
+++ components/Uploader/demos/avatar.js
@@ -0,0 +1,57 @@
+define([], function () {
+    return {
+        title: '自定义头像上传按钮样式',
+        file: 'avatar',
+        demo: function () {
+            let avatarRef = null
+            return {
+                children: {
+                    component: 'Uploader',
+                    accept: 'image/*',
+                    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+                    label: 'LOGO',
+                    type: 'avatar',
+                    display: 'replace',
+                    button: {
+                        classes: {
+                            'upload-img-button': true,
+                        },
+                        children: [{
+                            ref: (c) => {
+                                avatarRef = c
+                            },
+                            styles: {
+                                border: [true, 'primary'],
+                                padding: '1',
+                                shape: 'square',
+                            },
+                            component: 'Avatar',
+                            text: '头像',
+                            src: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                            size: 'xlarge',
+                        },]
+                    },
+                    onRemove: {
+                        action: () => {
+                            return new Promise((resolve) => {
+                                avatarRef.update({ src: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' })
+                                resolve(true)
+                            })
+                        },
+                    },
+                    onChange: function ({ file, fileList }) {
+                        // console.log(file, fileList)
+                        const { originFile } = file
+                        if (fileList && fileList.length > 0) {
+                            if (fileList[0].status === 'done' && originFile && originFile instanceof File) {
+                                const src = URL.createObjectURL(originFile)
+                                avatarRef.update({ src })
+                            }
+                        }
+
+                    },
+                },
+            }
+        },
+    }
+})
