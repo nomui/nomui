fcbbee9d4da446692a8434136692f175a7457155
Dan.Su <40448732+fatalisa@users.noreply.github.com>
2022-09-15 15:20:51 +0800

    fix(Grid): getData方法可能返回undefined

--- components/Grid/Grid.js
+++ components/Grid/Grid.js
@@ -736,7 +736,7 @@ class Grid extends Component {
     const keys = this.getDataKeys()
     const data = keys.map(function (key) {
       return that.props.data.filter(function (item) {
-        return parseInt(item[that.props.keyField], 10) === parseInt(key, 10)
+        return `${item[that.props.keyField]}` === `${key}`
       })[0]
     })
     return data
