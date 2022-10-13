e567797f1af40dfe2e9a14cdd1e248932591bcaf
luoquan <quan.luo@wetrial.com>
2022-10-13 11:42:59 +0800

    feat: Cascader 添加新功能，配置子项的disable属性

---
+++ components/Cascader/demos/disabled.js
@@ -0,0 +1,47 @@
+define(['./data.js'], function () {
+  return {
+    title: '禁用选项',
+    file: 'test',
+    demo: function () {
+      return {
+        component: 'Cascader',
+        placeholder: '请选择',
+        options: [
+          {
+            value: 'zhejiang',
+            label: 'Zhejiang',
+            children: [
+              {
+                value: 'hangzhou',
+                label: 'Hangzhou',
+                children: [
+                  {
+                    value: 'xihu',
+                    label: 'West Lake',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            value: 'jiangsu',
+            label: 'Jiangsu',
+            disabled: true,
+            children: [
+              {
+                value: 'nanjing',
+                label: 'Nanjing',
+                children: [
+                  {
+                    value: 'zhonghuamen',
+                    label: 'Zhong Hua Men',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      }
+    },
+  }
+})
