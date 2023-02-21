5c6df1a2d4d0533fa37a2f9bc9b31a2bc128cdfa
fatalisa <40448732+fatalisa@users.noreply.github.com>
2023-02-21 06:56:40 +0000

    Deploying to gh-pages from @ nomui/nomui@6dc92d13fe1f840ecc8c6e82f8a77d760a9655a5 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -13445,13 +13445,17 @@ function _defineProperty2(obj, key, value) {
       const {
         multiple,
         showArrow,
-        placeholder,
         disabled,
         showSearch,
         allowClear,
         options,
       } = this.props;
       const children = [];
+      let placeholder = this.props.placeholder;
+      if (!placeholder && (!Array.isArray(options) || !options.length)) {
+        this.props.value = "";
+        placeholder = "暂无数据";
+      }
       this._normalizeInternalOptions(options);
       this._normalizeSearchable();
       this.setProps({
