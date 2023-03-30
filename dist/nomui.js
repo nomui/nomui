bb82278448ba2e5c0aa6ea4ff122bc93960bc2da
jerry-nom <75375355+jerry-nom@users.noreply.github.com>
2023-03-30 07:33:47 +0000

    Deploying to gh-pages from @ nomui/nomui@4bc25b41b2f49ae942bf13928b0035a5395e0b7b 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -27015,7 +27015,7 @@ function _defineProperty2(obj, key, value) {
       }
       this._content.update({ children: this._getContentBadges() }); // 多选: 每次setValue后更新选中状态
       if (this.props.multiple) {
-        this.popup.selectControl.tree.checkedKeys(value);
+        this.popup.selectControl.tree.checkNodes(value);
       } else {
         // 单选: 点击后即关闭popup,在onShow中更新
         this.props.animate && this.popup.animateHide();
