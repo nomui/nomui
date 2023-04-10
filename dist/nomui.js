73b54d507d2de365052cbdc9da037a6ec0284553
fatalisa <40448732+fatalisa@users.noreply.github.com>
2023-04-10 01:32:59 +0000

    Deploying to gh-pages from @ nomui/nomui@dacdb69e375ce11c7ac772188cf09e4a717fa5c0 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -25,7 +25,7 @@ function _objectWithoutPropertiesLoose2(source, excluded) {
 }
 /**
  *
- *       nomui v1.2.1
+ *       nomui v1.2.2
  *       License: MIT
  *       Copyright (c) 2021-2021, Wetrial
  *
