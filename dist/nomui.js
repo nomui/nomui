4f5bfbc36c396a523844a3f36453039fc0d7bee8
fatalisa <40448732+fatalisa@users.noreply.github.com>
2024-09-11 08:55:49 +0000

    Deploying to gh-pages from @ nomui/nomui@223271d09298c631d1fba12afe6881a08a4612a9 🚀

--- dist/nomui.js
+++ dist/nomui.js
@@ -4439,6 +4439,9 @@ function _objectWithoutPropertiesLoose2(source, excluded) {
     }
     _rendered() {} // todo: 需要优化，现在循环删除节点，太耗时，计划改成只移除本节点，子节点只做清理操作
     remove() {
+      if (!this.props) {
+        return;
+      }
       const el = this._removeCore();
       this.parent &&
         this.parent.hasOwnProperty("removeChild") &&
@@ -17665,7 +17668,7 @@ function _objectWithoutPropertiesLoose2(source, excluded) {
     _config() {
       this.setProps({
         classes: { "nom-loading-icon": true, "status-fail": true },
-        children: `#<svg width="2rem" height="2rem" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <circle cx="50" cy="50" r="40" stroke="currentColor" stroke-width="6" fill="none" stroke-dasharray="251.2 251.2" stroke-dashoffset="251.2"><animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="0.4s" fill="freeze" /></circle><path d="M30,30 L70,70" stroke="currentColor" stroke-width="6" fill="none" stroke-dasharray="60" stroke-dashoffset="60" opacity="0"><animate attributeName="stroke-dashoffset" from="40" to="0" dur="0.3s" begin="0.4s" fill="freeze" /><animate attributeName="opacity" from="0" to="1" dur="0.3s" begin="0.4s" fill="freeze" /></path><path d="M70,30 L30,70" stroke="currentColor" stroke-width="6" fill="none" stroke-dasharray="60" stroke-dashoffset="60" opacity="0"><animate attributeName="stroke-dashoffset" from="40" to="0" dur="0.3s" begin="0.7s" fill="freeze" /><animate attributeName="opacity" from="0" to="1" dur="0.3s" begin="0.7s" fill="freeze" /></path></svg>`,
+        children: `#<svg width="2rem" height="2rem" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <circle cx="50" cy="50" r="40" stroke="var(--nom-color-danger)" stroke-width="6" fill="none" stroke-dasharray="251.2 251.2" stroke-dashoffset="251.2"><animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="0.4s" fill="freeze" /></circle><path d="M30,30 L70,70" stroke="var(--nom-color-danger)" stroke-width="6" fill="none" stroke-dasharray="60" stroke-dashoffset="60" opacity="0"><animate attributeName="stroke-dashoffset" from="40" to="0" dur="0.3s" begin="0.4s" fill="freeze" /><animate attributeName="opacity" from="0" to="1" dur="0.3s" begin="0.4s" fill="freeze" /></path><path d="M70,30 L30,70" stroke="var(--nom-color-danger)" stroke-width="6" fill="none" stroke-dasharray="60" stroke-dashoffset="60" opacity="0"><animate attributeName="stroke-dashoffset" from="40" to="0" dur="0.3s" begin="0.7s" fill="freeze" /><animate attributeName="opacity" from="0" to="1" dur="0.3s" begin="0.7s" fill="freeze" /></path></svg>`,
       });
     }
   }
@@ -17682,7 +17685,7 @@ function _objectWithoutPropertiesLoose2(source, excluded) {
     _config() {
       this.setProps({
         classes: { "nom-loading-icon": true, "status-success": true },
-        children: `#<svg width="2rem" height="2rem" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <!-- 圆环 --> <circle cx="50" cy="50" r="40" stroke="currentColor" stroke-width="6" fill="none" stroke-dasharray="251.2 251.2" stroke-dashoffset="251.2"> <animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="0.4s" fill="freeze" /> </circle> <path d="M25,50 L45,70 L75,35" stroke="currentColor" stroke-width="6" fill="none" stroke-dasharray="100" stroke-dashoffset="100" opacity="0"> <animate attributeName="stroke-dashoffset" from="100" to="0" dur="0.3s" begin="0.4s" fill="freeze" /> <animate attributeName="opacity" from="0" to="1" dur="0.6s" begin="0.4s" fill="freeze" /> </path> </svg>`,
+        children: `#<svg width="2rem" height="2rem" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <!-- 圆环 --> <circle cx="50" cy="50" r="40" stroke="var(--nom-color-success)" stroke-width="6" fill="none" stroke-dasharray="251.2 251.2" stroke-dashoffset="251.2"> <animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="0.4s" fill="freeze" /> </circle> <path d="M25,50 L45,70 L75,35" stroke="var(--nom-color-success)" stroke-width="6" fill="none" stroke-dasharray="100" stroke-dashoffset="100" opacity="0"> <animate attributeName="stroke-dashoffset" from="100" to="0" dur="0.3s" begin="0.4s" fill="freeze" /> <animate attributeName="opacity" from="1" to="1" dur="0.6s" begin="0.4s" fill="freeze" /> </path> </svg>`,
       });
     }
   }
