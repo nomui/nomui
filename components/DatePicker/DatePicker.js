0597dbbb4f1b2a98b21bc1dcd3b5314d1447c93b
fatalisa <40448732+fatalisa@users.noreply.github.com>
2025-03-14 08:47:56 +0000

    Deploying to gh-pages from @ nomui/nomui@173c1bc5d03e9de3fc7981f12a5fc77fcb510d8d 🚀

--- components/DatePicker/DatePicker.js
+++ components/DatePicker/DatePicker.js
@@ -332,6 +332,16 @@ class DatePicker extends Textbox {
                                 })
                               }
 
+                              if (that.props.weekMode) {
+                                this.weekCount = that._getWeekNumber(date)
+                                this.setProps({
+                                  tooltip: that.props.weekCountText.replace(
+                                    '{week}',
+                                    this.weekCount,
+                                  ),
+                                })
+                              }
+
                               this.setProps({
                                 styles: {
                                   text: 'center',
@@ -919,6 +929,65 @@ class DatePicker extends Textbox {
     }
   }
 
+  /**
+   * 计算传入日期属于哪一年的第几周
+   * @param {Date} date - 传入的日期
+   * @returns {Object} - 返回包含年份和周数的对象，如 { year: 2023, week: 12 }
+   */
+  _getWeekNumber(date) {
+    date = new Date(date)
+    const year = date.getFullYear() // 获取年份
+    const firstDayOfYear = new Date(year, 0, 1) // 本年1月1日
+    const lastDayOfYear = new Date(year, 11, 31) // 本年12月31日
+
+    const firstDayOfYearWeekday = firstDayOfYear.getDay()
+
+    const lastDayOfYearWeekday = lastDayOfYear.getDay()
+
+    // 根据 startWeekOnMonday 配置项调整周的计算逻辑
+    const isStartWeekOnMonday = this.props.startWeekOnMonday
+
+    // 计算第一周的起始日期
+    let firstWeekStart
+    if (isStartWeekOnMonday) {
+      if (firstDayOfYearWeekday === 1) {
+        firstWeekStart = new Date(year, 0, 1)
+      } else {
+        firstWeekStart = new Date(year, 0, 1 + ((1 - firstDayOfYearWeekday + 7) % 7))
+      }
+    } else if (firstDayOfYearWeekday === 0) {
+      firstWeekStart = new Date(year, 0, 1)
+    } else {
+      firstWeekStart = new Date(year, 0, 1 - firstDayOfYearWeekday)
+    }
+
+    let lastWeekEnd
+    if (isStartWeekOnMonday) {
+      if (lastDayOfYearWeekday === 0) {
+        lastWeekEnd = new Date(year, 11, 31)
+      } else {
+        lastWeekEnd = new Date(year, 11, 31 + (7 - lastDayOfYearWeekday))
+      }
+    } else if (lastDayOfYearWeekday === 6) {
+      lastWeekEnd = new Date(year, 11, 31)
+    } else {
+      lastWeekEnd = new Date(year, 11, 31 + (6 - lastDayOfYearWeekday))
+    }
+
+    if (date < firstWeekStart) {
+      return this.getWeekNumber(new Date(year - 1, 11, 31))
+    }
+
+    if (date > lastWeekEnd) {
+      return this.getWeekNumber(new Date(year + 1, 0, 1))
+    }
+
+    const timeDiff = date.getTime() - firstWeekStart.getTime()
+    const weekDiff = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000)) + 1
+
+    return weekDiff
+  }
+
   _getNowText() {
     if (this.props.weekMode) {
       return this.props.currentWeekText
@@ -1006,6 +1075,7 @@ DatePicker.defaults = {
   startWeekOnMonday: true, // 将周一算作一周的开始
   weekText: '日 一 二 三 四 五 六',
   currentWeekText: '本周',
+  weekCountText: `第{week}周`,
   nowText: '此刻',
   todayText: '今天',
   showYearSkip: false,
