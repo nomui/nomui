fcb585fd5bf828060d37749d8f7d15cf0f4330db
fatalisa <40448732+fatalisa@users.noreply.github.com>
2025-03-17 08:20:34 +0000

    Deploying to gh-pages from @ nomui/nomui@83082543fa7732af73c48cb72ee67ab3eb6cf8ab 🚀

--- components/DatePicker/demos/week-mode.js
+++ components/DatePicker/demos/week-mode.js
@@ -5,6 +5,7 @@ define([], function () {
     description:
       '周选择模式下，界面上显示的是以周为单位，但取值的日期为当周的第一天，如果传入format则以format作为格式，取值为某年某周（此时该取值不符合合法日期校验规则）。',
     demo: function () {
+      let dateRef = null
       return {
         children: {
           component: 'Flex',
@@ -23,10 +24,30 @@ define([], function () {
               weekMode: {
                 format: '{year}年{week}周',
               },
-              onValueChange: ({ sender }) => {
-                console.log(sender.getWeekDetails())
-              },
               value: '2025年22周',
+              ref: (c) => {
+                dateRef = c
+              },
+            },
+            {
+              justify: 'center',
+              gutter: 'medium',
+              cols: [
+                {
+                  component: 'Button',
+                  text: '获取周详情',
+                  onClick: () => {
+                    console.log(dateRef.getWeekDetails())
+                  },
+                },
+                {
+                  component: 'Button',
+                  text: '赋值',
+                  onClick: () => {
+                    console.log(dateRef.setValue('2024年1周'))
+                  },
+                },
+              ],
             },
           ],
         },
