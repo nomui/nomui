5e46505a6fea9c9a00770bcb34af007322a6778c
Dan <daniel.su@ctsmed.com>
2025-05-29 09:13:58 +0800

    wip: demo

--- components/Grid/demos/basic.js
+++ components/Grid/demos/basic.js
@@ -1,51 +1,249 @@
-define([], function () {
+define(['css!./style.css'], function () {
   return {
     title: '基础用法',
     file: 'basic',
     demo: function () {
+      const data = [
+        {
+          id: 1,
+          title: '工时名称1',
+          dates: [
+            {
+              date: '2023-10-01',
+              hours: 8,
+            },
+            {
+              date: '2023-10-02',
+              hours: 6,
+            },
+            {
+              date: '2023-10-03',
+              hours: 7,
+            },
+            {
+              date: '2023-10-04',
+              hours: 8,
+            },
+            {
+              date: '2023-10-05',
+              hours: 5,
+            },
+            {
+              date: '2023-10-06',
+              hours: 8,
+            },
+            {
+              date: '2023-10-07',
+              hours: 6,
+            },
+          ],
+          totolHour: 48,
+        },
+        {
+          id: 2,
+          title: '工时名称2',
+          dates: [
+            {
+              date: '2023-10-01',
+              hours: 7,
+            },
+            {
+              date: '2023-10-02',
+              hours: 8,
+            },
+            {
+              date: '2023-10-03',
+              hours: 6,
+            },
+            {
+              date: '2023-10-04',
+              hours: 7,
+            },
+            {
+              date: '2023-10-05',
+              hours: 8,
+            },
+            {
+              date: '2023-10-06',
+              hours: 5,
+            },
+            {
+              date: '2023-10-07',
+              hours: 8,
+            },
+          ],
+          totolHour: 49,
+        },
+        {
+          id: 3,
+          title: '工时名称3',
+          dates: [
+            {
+              date: '2023-10-01',
+              hours: null, // 允许为空
+            },
+            {
+              date: '2023-10-02',
+              hours: 7,
+            },
+            {
+              date: '2023-10-03',
+              hours: 8,
+            },
+            {
+              date: '2023-10-04',
+              hours: 6,
+            },
+            {
+              date: '2023-10-05',
+              hours: 7,
+            },
+            {
+              date: '2023-10-06',
+              hours: 8,
+            },
+            {
+              date: '2023-10-07',
+              hours: 5,
+            },
+          ],
+          totolHour: 47,
+        },
+      ]
       return {
         component: 'Grid',
-        showTitle: true,
-        onRowClick: (args) => {
-          console.log('row clicked', args)
+        editable: true,
+        classes: {
+          'wt-work-hour-grid': true,
         },
-        rowSelectable: {
-          onSelect: (args) => {
-            console.log(args)
-          },
-        },
-
         columns: [
           {
-            field: 'name',
-            key: 'name',
-            title: '标题',
+            field: 'title',
+            title: '任务名称',
             width: 200,
           },
           {
-            field: 'author',
-            key: 'author',
-            title: '作者',
+            field: 'day1',
+            title: '10月1日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[0].hours || 0}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[0].hours,
+              }
+            },
           },
           {
-            field: 'sales',
-            key: 'sales',
-            title: '销量',
+            field: 'day2',
+            title: '10月2日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[1].hours || 0}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[1].hours,
+              }
+            },
           },
-
           {
-            field: 'role',
-            key: 'role',
-            title: '主角',
-            width: 500,
-            showTitle: false,
+            field: 'day3',
+            title: '10月3日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[2].hours}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[2].hours,
+              }
+            },
+          },
+          {
+            field: 'day4',
+            title: '10月4日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[3].hours}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[3].hours,
+              }
+            },
+          },
+          {
+            field: 'day5',
+            title: '10月5日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[4].hours}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[4].hours,
+              }
+            },
+          },
+          {
+            field: 'day6',
+            title: '10月6日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[5].hours}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[5].hours,
+              }
+            },
+          },
+          {
+            field: 'day7',
+            title: '10月7日',
+            width: 120,
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.dates[6].hours}h`
+            },
+            editRender: ({ rowData }) => {
+              return {
+                component: 'NumberInput',
+                step: 0.5,
+                value: rowData.dates[6].hours,
+              }
+            },
+          },
+          {
+            field: 'totolHour',
+            title: '总工时',
+            align: 'center',
+            cellRender: ({ rowData }) => {
+              return `${rowData.totolHour}h`
+            },
           },
         ],
-        data: [
-          { id: 1, name: '笑傲江湖', author: '金庸', sales: 100000, role: '令狐冲' },
-          { id: 4, name: '天龙八部', author: '金庸', sales: 200000, role: '乔峰' },
-          { id: 5, name: '射雕英雄传', author: '金庸', sales: 0, role: '郭靖' },
-        ],
+        data: data,
       }
     },
   }
