8edaa6cbd63adf4acfb782dd4e073c352ace098a
Dan <daniel.su@ctsmed.com>
2025-06-10 13:40:05 +0800

    docs: 文档更新

--- components/MultilineTextbox/MultilineTextbox.js
+++ components/MultilineTextbox/MultilineTextbox.js
@@ -41,7 +41,7 @@ class MultilineTextbox extends Field {
     if (this.props.onEnter) {
       this.textarea._on('keydown', function (event) {
         if (event.keyCode && event.keyCode === 13) {
-          that._callHandler(that.props.onEnter, { value: that.getValue() })
+          that._callHandler(that.props.onEnter, { value: that.getValue(), event })
         }
       })
     }
