b0fca23303b4a86662d5b1b9c47adf201362c6d6
Dan <daniel.su@ctsmed.com>
2025-08-11 18:20:32 +0800

    feat(Avatar): 图片支持数组

--- components/Avatar/demos/with-img.js
+++ components/Avatar/demos/with-img.js
@@ -1,7 +1,8 @@
 define([], function () {
   return {
     title: '带图片',
-    description: '使用img图片，优先级高于icon',
+    description:
+      '使用img图片，优先级高于icon,src可以传入一个字符串，也可以传入字符串数组，根据优先级取第一个能访问的图片',
     file: 'with-img',
     demo: function () {
       return {
@@ -26,7 +27,10 @@ define([], function () {
             component: 'Avatar',
             icon: 'user',
             text: '小马',
-            src: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            src: [
+              'https://pic2.zhimg.com/80/v2-0fb7738fb1c8ed2e4c0782a8344fa9b1_r.jpg',
+              'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            ],
             size: 'xlarge',
           },
         ],
--- components/Avatar/index.md
+++ components/Avatar/index.md
@@ -8,7 +8,7 @@
 | gap    | 字符类型距离左右两侧边界单位像素 | `number`                          | 4       |
 | icon   | 设置头像的自定义图标             | `string`                          | -       |
 | size   | 设置头像的大小                   | `number \| xs \| sm \| lg \| xl ` | default |
-| src    | 图片类头像的资源地址或者图片元素 | `string `                         | -       |
+| src    | 图片类头像的资源地址或者图片元素 | `string \| array`                 | -       |
 | text   | 头像显示的内容                   | `string `                         | -       |
 | styles | 指定头像外形                     | `StyleProps`                      | -       |
 
